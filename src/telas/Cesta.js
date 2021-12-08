b5bb266ad73c212043aa421d6b94176547f495c6
Yuri <yuricoelho33@gmail.com>
2021-12-07 22:04:10 -0300

    Refatorando o código.

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
 
+import Texto from '../components/Texto';
+
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
 
@@ -12,15 +14,15 @@ export default function Cesta() {
     <Text style={estilos.titulo}>Detalhe da cesta</Text>
 
     <View style={estilos.cesta}>
-        <Text style={estilos.nome}>Cesta de Verduras</Text>
+        <Texto style={estilos.nome}>Cesta de Verduras</Texto>
         <View style={estilos.fazenda}>
             <Image source={logo} style={estilos.nomeFazenda} />
-            <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
         </View>
-        <Text style={estilos.descricao}>Uma cesta com produtos selecionados 
+        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados 
               cuidadosamente da fazenda direto 
-              para sua cozinha</Text>
-        <Text style={estilos.preco}>R$ 40,00</Text>      
+              para sua cozinha</Texto>
+        <Texto style={estilos.preco}>R$ 40,00</Texto>      
     </View>
     </>
 }
